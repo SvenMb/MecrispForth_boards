5471854686bec4017560f2d54213cd0658122290
Sven Muehlberg <SvenMuehlberg@gmail.com>
2021-05-25 00:49:18 +0200

    new wording scheme
    first working send routine for nrf24

--- stm32f407_blackboard/lib/nrf24.fs
+++ stm32f407_blackboard/lib/nrf24.fs
@@ -7,14 +7,16 @@
 
 (   nrf24 start: ) here dup hex.
 
+[ifndef] spi1-init 
 include spi1.fs
+[then]
+[ifndef] nrf24.cmd
 include nrf24l01.fs
-
-0 variable nrf24_pmodel
+[then]
 
 \ check if nrf24l01 is there, needs nrf24.pininit before
 : nrf24.isConnected ( -- bool )
-    $03 nrf24_read_register drop
+    nrf24.SETUP_AW nrf24.r_cmd drop
     dup 0 > swap 4 < and \ should be between 1 and 3 if nrf24l01 on spi 
 ;
 
@@ -29,7 +31,7 @@ include nrf24l01.fs
 \ initializes nrf24 with basic settings
 : nrf24.init
     \ hardware setup
-    nrf24_pininit
+    nrf24.pininit
     
     5 ms \ delay for settle nrf24
     
@@ -39,97 +41,97 @@ include nrf24l01.fs
         CR ." no nRF24L01 found!"
         exit
     then
-    
     \ setRetries 
     $f 4 lshift \ delay 4ms
     $f or       \ retransmits 15
-    $04 nrf24_write_register drop \ SETUP_RETR
-    
+    nrf24.SETUP_RETR nrf24.w_register drop \ SETUP_RETR
     \ setDataRate
-    $06 nrf24_read_register drop \ RF_SETUP
+    nrf24.RF_SETUP nrf24.r_register drop \ RF_SETUP
+
     $08 $20 or not and \ mask RF_DR_HIGH and RF_DR_LOW (p-model)
     \ $00 or ( 1 MBPS )
     \ $08 or ( 2 MBPS )
     \ $20 or ( 250kBPS only p-model )
-    dup $06 nrf24_write_register drop
-    $06 nrf24_read_register drop
+    dup nrf24.RF_SETUP nrf24.w_register drop
+    nrf24.RF_SETUP nrf24.r_register drop
     <> if     \ set speed failed
         CR ." setup nRF24 speed failed"
         exit
     then
 
     \ detect p-model and set features
-    $1d nrf24_read_register drop
+    nrf24.FEATURE nrf24.r_register drop
+    
     \ toggle feature
-    $73 $50 nrf24_write_register drop  \ toggle features 
-    \ read feature
+    nrf24.toggle_feature drop  \ toggle features 
 
-    $1d nrf24_read_register drop
+    \ read feature
+    nrf24.FEATURE nrf24.r_register drop
     dup -rot \ save actual features
     \ if old features and actual features same then p-model
     = if
-         1 nrf24_pmodel !
+        $01 nrf24.stat cbis!
+    else
+        $01 nrf24.stat cbic!
     then
     
     0<> if \ features set, take care
-        nrf24_pmodel @ if
-            $73 $50 nrf24_write_register drop \ toggle features 
+        $01 nrf24.stat cbit@ if
+            nrf24.toggle_feature drop \ toggle features 
         then
-        $0 $1d nrf24_write_register drop
+        $0 nrf24.FEATURE nrf24.w_register drop
     then
     \ features is now 0
 
     \ setDatarate to 250kBPS if p-model
-    nrf24_pmodel @ if
-        $06 nrf24_read_register drop \ RF_SETUP
+    $01 nrf24.stat cbit@ if
+        nrf24.RF_SETUP nrf24.r_register drop \ RF_SETUP
         $08 not and $20 or ( 250kBPS only p-model )
-        $06 nrf24_write_register drop
+        nrf24.RF_SETUP nrf24.w_register drop
     then
     
-    $04 $1d nrf24_write_register drop \ set EN_DPL on FEATURE
-    $3f $1c nrf24_write_register drop \ set dynPayload on all pipes 
+    $04 nrf24.FEATURE nrf24.w_register drop \ set EN_DPL on FEATURE
+    $3f nrf24.DYNPD nrf24.w_register drop \ set dynPayload on all pipes 
 
-    \ $00 $1c nrf24_write_register drop \ set DYNPD 0
+    $3f nrf24.EN_AA nrf24.w_register drop \ set EN_AA - auto ack on all pipes
     
-    $3f $01 nrf24_write_register drop \ set EN_AA - auto ack on all pipes
-    
-    $03 $02 nrf24_write_register drop \ set EN_RXADDR - RX pipe 1+2
+    $03 nrf24.EN_RXADDR nrf24.w_register drop \ set EN_RXADDR - RX pipe 1+2
     
     \ set payload size, statisch 32 byte for all 6 pipes
     6 0 do
         32      \ 32 bytes
-        $11 i + \ calc RX_PW_Px register
-        nrf24_write_register drop
+        nrf24.RX_PW_P0 i + \ calc RX_PW_Px register
+        nrf24.w_register drop
     loop
 
     \ address width 5 bytes
-    $03 $03 nrf24_write_register drop \ set SETUP_AW
+    $03 nrf24.SETUP_AW nrf24.w_register drop \ set SETUP_AW
     
     \ channel address (max 125)
-    100 $05 nrf24_write_register drop \ set RF_CH
+    100 nrf24.RF_CH nrf24.w_register drop \ set RF_CH
     
     \ reset RX and TX fifo also retransmits 
-    $70 $07 nrf24_write_register drop \ set STATUS
+    $70 nrf24.STATUS nrf24.w_register drop \ set STATUS
     
     \ switch off irqs, power down , crc 16bit, 
-    $7C $00 nrf24_write_register drop
+    $7C nrf24.CONFIG nrf24.w_register drop
 
     \ set Addr to "1SNSR"
     s" 1SNSR" 2dup
-    $0a nrf24_write_registers drop \ write to RX_ADDR_P0
-    $10 nrf24_write_registers drop \ write to TX_ADDR
+    nrf24.RX_ADDR_P0 nrf24.w_registers drop \ write to RX_ADDR_P0
+    nrf24.TX_ADDR nrf24.w_registers drop    \ write to TX_ADDR
 
     \ stop listen
-    0 nrf24_read_register drop
+    nrf24.CONFIG nrf24.r_register drop
     $01 not and \ reset CONFIG.PRIM_RX
-    0 nrf24_write_register drop
+    nrf24.CONFIG nrf24.w_register drop
 ;
 
 \ print out registers
 : nrf24.stat. ( -- )
-    CR ." === NRF24L01" nrf24_pmodel @ if [char] + emit then ."  ==="
+    CR ." === NRF24L01" $01 nrf24.stat cbit@ if [char] + emit then ."  ==="
     CR ." STATUS:     "
-    $07 nrf24_read_register drop dup h.2
+    $07 nrf24.r_register drop dup h.2
     CR ."     RX_DR: " dup 6 rshift 1 and .
     ." TX_DS: " dup 5 rshift 1 and .
     ." MAX_RT: "  dup 4 rshift 1 and .
@@ -137,7 +139,7 @@ include nrf24l01.fs
     ." TX_FULL: " 1 and .
     
     CR ." SETUP_AW:   " 
-    $03 nrf24_read_register drop dup h.2
+    $03 nrf24.r_register drop dup h.2
     dup 0 > swap 4 < and not
     if
         CR ." no NRF24L01 found!"
@@ -145,59 +147,59 @@ include nrf24l01.fs
     then
 
     CR ." CONFIG:     "
-    $00 nrf24_read_register drop h.2
+    $00 nrf24.r_register drop h.2
     
     CR ." EN_AA:      "
-    $01 nrf24_read_register drop h.2
+    $01 nrf24.r_register drop h.2
 
     CR ." EN_RXADDR:  "
-    $02 nrf24_read_register drop h.2
+    $02 nrf24.r_register drop h.2
 
     CR ." SETUP_RETR: "
-    $04 nrf24_read_register drop h.2
+    $04 nrf24.r_register drop h.2
 
     CR ." RF_CH:      "
-    $05 nrf24_read_register drop h.2
+    $05 nrf24.r_register drop h.2
 
     CR ." RF_SETUP:   "
-    $06 nrf24_read_register drop h.2
+    $06 nrf24.r_register drop h.2
 
     CR ." OBSERVE_TX: "
-    $08 nrf24_read_register drop h.2
+    $08 nrf24.r_register drop h.2
 
     CR ." RPD (CD):   "
-    $09 nrf24_read_register drop h.2
+    $09 nrf24.r_register drop h.2
 
     \ RX Addrs
     6 0 do
         CR ." RX_ADDR_P" i .digit emit ." : "
         i 2 < if \ special treatment P0/P1
-            nrf24_buf 5 $0a i + nrf24_read_registers drop
-            5 0 do nrf24_buf i + c@ h.2 loop
+            nrf24.buf 5 $0a i + nrf24.r_registers drop
+            5 0 do nrf24.buf i + c@ h.2 loop
             SPACE [char] " emit
-            5 0 do nrf24_buf i + c@ 32 max 126 min emit loop
+            5 0 do nrf24.buf i + c@ 32 max 126 min emit loop
             [char] " emit
         else
-            $0a i + nrf24_read_register drop h.2
+            $0a i + nrf24.r_register drop h.2
         then
 
     loop
 
     \ TX Addr
     CR ." TX_ADDR:    "
-    nrf24_buf 5 $0a i + nrf24_read_registers drop
-    5 0 do nrf24_buf i + c@ h.2 loop
+    nrf24.buf 5 $0a i + nrf24.r_registers drop
+    5 0 do nrf24.buf i + c@ h.2 loop
     SPACE [char] " emit
-    5 0 do nrf24_buf i + c@ 32 max 126 min emit loop
+    5 0 do nrf24.buf i + c@ 32 max 126 min emit loop
     [char] " emit
 
     CR ." RX_PW_P0-5: "
     6 0 do
-        $11 i + nrf24_read_register drop h.2 space
+        $11 i + nrf24.r_register drop h.2 space
     loop
     
     CR ." FIFO_STATUS:"
-    $17 nrf24_read_register drop dup h.2
+    $17 nrf24.r_register drop dup h.2
     CR ."     TX_REUSE: " dup 6 rshift 1 and .
     ." TX_FULL: " dup 5 rshift 1 and .
     ." TX_EMPTY: " dup 4 rshift 1 and .
@@ -205,15 +207,15 @@ include nrf24l01.fs
     ." RX_EMPTY: " 1 and .
     
     CR ." DYNPD:      "
-    $1C nrf24_read_register drop h.2
+    $1C nrf24.r_register drop h.2
 
     CR ." FEATURE:    "
-    $1D nrf24_read_register drop h.2
+    $1D nrf24.r_register drop h.2
 
 ;
 
-: nrf24.buf.
-    nrf24_buf
+: nrf24.buf. ( -- )
+    nrf24.buf
     32 0 do
         i 8 mod 0= if CR then
         dup i + c@ h.2 space
@@ -221,13 +223,76 @@ include nrf24l01.fs
     drop
 ;
 
+: nrf24.powerUp ( -- )
+    nrf24.CONFIG nrf24.r_register drop  \ read CONFIG
+    dup $03 and not if  \ if not already powerup
+        $02 or      \ set PWR_UP
+        nrf24.CONFIG nrf24.w_register drop  \ write CONFIG
+        5 ms \ power up takes time
+    else
+        drop
+    then
+;
+
+: nrf24.powerDown ( -- )
+    nrf24.CONFIG nrf24.r_register drop  \ read CONFIG
+    dup $03 and if \ if not already TX and powerup
+        $02 bic    \ reset PWR_UP
+        nrf24.CONFIG nrf24.w_register drop  \ write CONFIG
+    else
+        drop
+    then
+;
+
+
+: nrf24.send ( buffer len -- status )
+    -nrf24.CE   \ make sure CE is low (not TX)
+
+    \ wait until free space in TX-queue
+    begin
+        nrf24.STATUS nrf24.r_register drop
+        $01 and
+    while
+            pause
+    repeat
+
+    \ clear TX_DS (ack) if set
+    nrf24.STATUS nrf24.r_register drop
+    $20 and if
+        $20 nrf24.STATUS nrf24.w_register drop  \ reset TX_DS
+    then
+
+    nrf24.powerUp
+
+    \ check if in TX mode
+    nrf24.CONFIG nrf24.r_register drop  \ read CONFIG
+    dup $01 and if \ if not already TX and powerup
+        $01 bic     \ reset PRIM_RX
+        nrf24.CONFIG nrf24.w_register drop  \ write CONFIG
+        5 ms \ switching to TX takes time?
+    else
+        drop
+    then
+    
+    nrf24.flush_tx drop                 \ flush
+
+    nrf24.w_tx_payload drop \ write payload
+
+    +nrf24.CE     \ SEND!
+
+    10 us
+
+    -nrf24.CE     \ done
+;
+
 (   nrf24   end: ) here dup hex.
 (   nrf24  size: ) swap - hex.
 
-\ CR
-\ nrf24_pininit
-\ nrf24.stat.
-\ CR CR 
+\ just for testing
+CR
+nrf24.pininit
+nrf24.stat.
+CR CR 
 nrf24.init
 CR nrf24.stat.
 
--- stm32f407_blackboard/lib/nrf24l01.fs
+++ stm32f407_blackboard/lib/nrf24l01.fs
@@ -7,95 +7,258 @@
 
 (   nrf24init start: ) here dup hex.
 
-$20 buffer: nrf24_buf
+[ifndef] spi1-init
+include spi1.fs
+[then]
+
+\ you will need a buffer anyway...
+$20 buffer: nrf24.buf
+
+\ stat about dev
+0 variable nrf24.stat
+\ 7:1 reserved
+\ 0 P-model
 
 \ extra pins for nrf24  
-PB7 constant nrf24-CS
-PB6 constant nrf24-CE
-PB8 constant nrf24-IRQ
+PB7 constant nrf24.CS
+PB6 constant nrf24.CE
+PB8 constant nrf24.IRQ
 
 \ initialize nrf24
-: nrf24_pininit ( -- )
+: nrf24.pininit ( -- )
     \ asuming spi1
     spi1-init
     
     \ asuming GPIOBEN already set
-    omode-pp nrf24-CS io-mode!  \ PB12 -> output for CS
-    nrf24-CS ios!               \ deselect spi2-touch
+    omode-pp nrf24.CS io-mode!  \ PB12 -> output for CS
+    nrf24.CS ios!               \ deselect spi2-touch
 
-    omode-pp nrf24-CE io-mode!
-    nrf24-CE ioc! \ set CE to low(!)
+    omode-pp nrf24.CE io-mode!
+    nrf24.CE ioc! \ set CE to low(!)
 
     \ clean buffer
-    nrf24_buf $20 $00 fill
+    nrf24.buf $20 $00 fill
 ;
 
 \ select spi to nrf24
 : +nrf24
-    nrf24-CS ioc! \ select nrf24 spi (CS low)
+    nrf24.CS ioc! \ select nrf24 spi (CS low)
 ;
 
 \ deselect spi to nrf24
 : -nrf24
-    nrf24-CS ios! \ deselect nrf24 spi (CS high)
+    nrf24.CS ios! \ deselect nrf24 spi (CS high)
 ;
 
 \ power up
 : +nrf24.CE
-    nrf24-CE ios!
+    nrf24.CE ios!
 ;
 
 \ power down
 : -nrf24.CE
-    nrf24-CE ioc!
+    nrf24.CE ioc!
 ;
 
-\ nrf24 write register
-: nrf24_write_register ( byte reg -- status )
+\ ==== basic command interface to nRF24 ====
+\ just send 1 byte cmd
+: nrf24.cmd ( cmd -- status )
     +nrf24
-    $20 or >spi1> \ W_REGISTER
-    swap          \ STATUS back
-    \ CR .s
-    >spi1> drop         \ send value
+    >spi1> 
     -nrf24
 ;
 
-\ nrf24 write register with multi byte from buffer
-: nrf24_write_registers ( buffer len reg -- status )
+\ send cmd and send 1 byte
+: nrf24.w_cmd ( byte cmd -- status )
     +nrf24
-    $20 or >spi1> \ send W_REGISTER get STATUS
-    >r            \ STATUS to return stack
+    >spi1> \ send cmd, get status
+    swap   \ STATUS back
+    >spi1  \ send value
+    -nrf24
+;
+
+\ send cmd and buffer (with len bytes)
+: nrf24.wb_cmd ( buffer len cmd -- status )
+    +nrf24
+    >spi1> \ send cmd, get STATUS
+    >r     \ STATUS to return stack
     0 do
         dup i + c@
-        >spi1> drop
+        >spi1
     loop
     -nrf24
     drop \ drop buffer
     r>   \ get STATUS back
 ;
 
-\ nrf24 read register
-: nrf24_read_register ( reg -- byte status)
+\ send cmd and get 1 byte
+: nrf24.r_cmd ( cmd -- byte status )
     +nrf24
-    >spi1> \ send R_REGISTER get STATUS
+    >spi1> \ send cmd get STATUS
     spi1>  \ Value
     -nrf24
     swap
 ;
 
-\ nrf24 read register with multi byte to buffer
-: nrf24_read_registers ( buffer len reg -- status )
+\ send cmd and get len bytes to buffer 
+: nrf24.rb_cmd ( buffer len cmd -- status )
     +nrf24
-    >spi1> \ send R_REGISTER get STATUS
+    >spi1> \ send cmd, get STATUS
     >r     \ save STATUS to return stack
     0 do            \ len times
         spi1>       \ get byte
-        \ CR .s
         over i + c!  \ save byte into buffer
     loop
     -nrf24
     drop    \ drop buffer
     r>      \ get STATUS back
+;
+
+\ start public:
+\ ==== Register names of nRF24
+$00 constant nrf24.CONFIG
+\ 7 reserved 
+\ 6 MASK_RX_DR
+\ 5 MASK_TX_DS
+\ 4 MASK_MAX_RT
+\ 3 EN_CRC
+\ 2 CRCO
+\ 1 PWR_UP
+\ 0 PRIM_RX
+$01 constant nrf24.EN_AA
+$02 constant nrf24.EN_RXADDR
+$03 constant nrf24.SETUP_AW
+\ 7:2 reserved ( all 0 )
+\ 1:0 addr length ( +2 )
+$04 constant nrf24.SETUP_RETR
+\ 7:4 auto retransmit delay ( $F = 4ms )  
+$05 constant nrf24.RF_CH
+\ 6:0 channel to operate on
+$06 constant nrf24.RF_SETUP
+\ 7 cont_wave
+\ 6 reserved
+\ 5 RF_DR_LOW
+\ 4 PLL_LOCK
+\ 3 RF_DR_HIGH
+\ 2:1 RF_PWR
+\ 0 obsolete
+$07 constant nrf24.STATUS
+\ 7 reserved
+\ 6 TX_DR
+\ 5 TX_DS
+\ 4 MAX_RT
+\ 3:1 RX_P_NO
+\ 0 TX_FULL
+$08 constant nrf24.OBSERVE_TX
+\ 7:4 PLOS_CNT
+\ 3:0 ARC_CNT
+$09 constant nrf24.CD
+\ 7:1 reserved
+\ 0 RPD ( CD - Carrier detect )
+$0a constant nrf24.RX_ADDR_P0   \ max 5 bytes
+\ $0b constant nrf24.RX_ADDR_P1 \ max 5 bytes
+\ $0c constant nrf24.RX_ADDR_P2 \ 1 byte
+\ $0d constant nrf24.RX_ADDR_P3 \ 1 byte 
+\ $0e constant nrf24.RX_ADDR_P4 \ 1 byte
+\ $0f constant nrf24.RX_ADDR_P5 \ 1 byte
+$10 constant nrf24.TX_ADDR      \ max 5 bytes
+$11 constant nrf24.RX_PW_P0
+\ 7:6 reserved
+\ 5:0 bytes in RX payload
+\ $12 constant nrf24.RX_PW_P1
+\ $13 constant nrf24.RX_PW_P2
+\ $14 constant nrf24.RX_PW_P3
+\ $15 constant nrf24.RX_PW_P4
+\ $16 constant nrf24.RX_PW_P5
+$17 constant nrf24.FIFO_STATUS
+\ 7 reserved
+\ 6 TX_REUSE
+\ 5 TX_FULL
+\ 4 TX_EMPTY
+\ 3:2 reserved
+\ 1 RX_FULL
+\ 0 RX_EMPTY
+$1c constant nrf24.DYNPD
+\ 7:6 reserved
+\ 5:0 bitmap dynamic payload pipes 
+$1d constant nrf24.FEATURE
+\ 7:3 reserved
+\ 2 EN_DPL ( enable dynamic payload )
+\ 1 EN_ACK_PAY ( enable auto ack )
+\ 0 EN_DYN_ACK ( enable W_TX_PAYLOAD_NOACK )
+
+\ ==== SPI command set nRF24L01 ==== 
+\ nrf24 read register 1 byte
+: nrf24.r_register ( reg -- byte status)
+    nrf24.r_cmd \ inline
+;
+
+\ nrf24 read register with multi byte to buffer
+: nrf24.r_registers ( buffer len reg -- status )
+    nrf24.rb_cmd \ inline
+;
+
+\ nrf24 write register 1 byte
+: nrf24.w_register ( byte reg -- status )
+    $20 or nrf24.w_cmd \ inline
+;
+
+\ nrf24 write register with multi byte from buffer
+: nrf24.w_registers ( buffer len reg -- status )
+    $20 or nrf24.wb_cmd \ inline
+;
+
+\ nrf24 read payload to buffer
+\ buffer should to be 32 byte, 
+: nrf24.r_rx_payload ( buffer len -- status )
+    $61 nrf24.rb_cmd \ inline
+;
+
+\ nrf24 write payload to buffer
+: nrf24.w_tx_payload ( buffer len -- status ) 
+    $A0 nrf24.wb_cmd \ inline
+;
+
+\ 
+: nrf24.flush_tx ( -- status )
+    $e1 nrf24.cmd \ inline
+;
+
+\
+: nrf24.flush_rx ( -- status )
+    $e2 nrf24.cmd \ inline
+;
+
+\ 
+: nrf24.reuse_tx_pl ( -- status )
+    $e3 nrf24.cmd \ inline
+;
+
+
+\ read rx payload width from current (top) payload entry
+: nrf24.r_rx_pl_wid ( -- width status ) 
+    $60 nrf24.r_cmd \ inline
+;
+
+\
+: nrf24.w_ack_payload ( buffer len #pipe -- status )
+    \ %101 max \ max #pipe $5 ??? 
+    $a4 or   \ blend in #pipe
+    nrf24.wb_cmd \ inline
+;
+
+\
+: nrf24.w_tx_payload_noack ( buffer len -- status )
+    $b0 nrf24.wb_cmd \ inline
+;
+
+\ no operation, but get status
+: nrf24.nop ( -- status )
+    $ff nrf24.cmd \ inline
+;
+
+: nrf24.toggle_feature ( -- status )
+    $73 $50 nrf24.w_cmd \ inline
 ;
 
 (   nrf24init   end: ) here dup hex.
